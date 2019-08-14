90d6ad67b19a855e47e1f264231046303dbcc154
189884 <189884@DevOpsLabApp>
2019-08-14 15:25:42 +0530

    Booking Java
    Conflicts:
    	src/I3/Classes/Booking.java

--- src/I3/Classes/Booking.java
+++ src/I3/Classes/Booking.java
@@ -9,7 +9,7 @@ import java.util.Date;
  */
 public class Booking {
     
-   // int CONFIRMED = 0; Test RajatGpta
+   // int CONFIRMED = 0; Test RajatGpta154
    // int RESERVED = 1;   
     
     // required Object
